d061f650671cc7aa28946165529d9c06e0289ecd
Marcos Curtidor <3048652220116@ingenieria.usac.edu.gt>
2024-02-29 20:00:33 -0600

    commit1

---
+++ mylibrary/src/main/java/mcurtidor/newlib/Class1.java
@@ -0,0 +1,8 @@
+package mcurtidor.newlib;
+
+public class Class1 {
+    
+    public static void aMethod() {
+        System.out.println("Hello from Class1!");
+    }
+}
---
+++ mylibrary/src/main/java/mcurtidor/newlib/Main.java
@@ -0,0 +1,11 @@
+package mcurtidor.newlib;
+
+public class Main {
+
+    public static void HelloWorld() {
+        System.out.println("Hello from Main!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
